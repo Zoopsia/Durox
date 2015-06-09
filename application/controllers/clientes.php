3f931ecfc6a2545ec715e62ab8ca1edd148526f4
Zoopsia <cristiannemesis101@hotmail.com>
2015-06-09 12:14:52 -0300

    message

---
+++ application/controllers/clientes.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Clientes extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->database();
+		$this->load->helper('url');
+
+		$this->load->library('grocery_CRUD');
+	}
+
+	public function prueba(){
+		
+		$this->load->view("head.php");
+	}
+	
+}
