e85dc1cd956bfc6756ced5f12743509894dbe850
Zoopsia <cristiannemesis101@hotmail.com>
2015-07-23 17:27:39 -0300

    1.1.82

--- libraries/main/js/main.js
+++ libraries/main/js/main.js
@@ -1,17 +1,5 @@
 $(document).ready(function(){
-	
-	$(window).scroll(function(){
-            if ($(this).scrollTop() > 10) {
-                $('.scrollup').fadeIn();
-            } else {
-                $('.scrollup').fadeOut();
-            }
-        });
-  
-	$('.scrollup').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 600);
-            return false;
-    });
+
 	
     $('.prueba').DataTable({
 
